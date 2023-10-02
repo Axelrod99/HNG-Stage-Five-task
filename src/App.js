e79c9909f2078cfc456390f5d67b8fd154c16c9a
jibola <ajibolaajiboye99@gmail.com>
2023-10-02 11:09:41 +0100

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route,} from "react-router-dom";
+import LandingPage from "./pages/LandingPage/landingPage";
+import HomePage from "./pages/home-page/Home-page";
+import RecorderMenu from "./components/recorderMenu";
+import VideoPlayer from "./pages/VideoPage/VideoPage";
+import AuthPage from "./pages/AuthPage";
+import Home from "./pages/Home/Home";
+import Create from './pages/CreatePage/Create'
+import Ready from './pages/Ready/Ready'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<AuthPage/>} />
+        <Route path="/ready" element={<VideoPlayer/>} />
+        <Route path="/landing-page" element={<LandingPage />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/ready" element={<Ready />} />
+      </Routes>
+    </Router>
+  ) 
 }
 
 export default App;
